2a87a76be55d93043e9eaa5b6f151ed58c656cc6
olympiawoj <owojcik7@gmail.com>
2020-08-14 16:05:07 -0400

    5. Use React Testing Library to Render and Test React Components

--- src/__tests__/react-testing-library.js
+++ src/__tests__/react-testing-library.js
@@ -1,3 +1,21 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import {getQueriesForElement} from '@testing-library/dom'
+// import {FavoriteNumber} from '../favorite-number'
+
+// function render(ui){
+//   const container = document.createElement('div')
+//   ReactDOM.render(ui, container)
+//   const queries = getQueriesForElement(container)
+//   return {container, ...queries}
+// }
+
+// test('renders a number input with a label "Favorite Number"', () => {
+//   const {getByLabelText} = render(<FavoriteNumber />)
+//   const input = getByLabelText(/favorite number/i)
+//   expect(input).toHaveAttribute('type', 'number')
+// })
+
 import React from 'react'
 import {render} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
