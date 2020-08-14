a2088b4b79189e1ce34a799ea47bd9a865a3c77d
olympiawoj <owojcik7@gmail.com>
2020-08-14 16:11:01 -0400

    6. Debug the DOM State During Tests using React Testing Library's debug Function

--- src/__tests__/react-testing-library.js
+++ src/__tests__/react-testing-library.js
@@ -21,7 +21,8 @@ import {render} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
 test('renders a number input with a label "Favorite Number"', () => {
-  const {getByLabelText} = render(<FavoriteNumber />)
+  const {getByLabelText, debug} = render(<FavoriteNumber />)
   const input = getByLabelText(/favorite number/i)
   expect(input).toHaveAttribute('type', 'number')
+  debug(input)
 })
