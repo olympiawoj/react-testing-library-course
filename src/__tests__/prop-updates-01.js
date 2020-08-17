68d54d476fd56dd1f8498e7a55ab9a07e3114278
olympiawoj <owojcik7@gmail.com>
2020-08-17 14:39:20 -0400

    9. Test Prop Updates with React Testing Library

--- src/__tests__/prop-updates-01.js
+++ src/__tests__/prop-updates-01.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import user from '@testing-library/user-event'
 import {render} from '@testing-library/react'
+import user from '@testing-library/user-event'
 import {FavoriteNumber} from '../favorite-number'
 
 test('entering an invalid value shows an error message', () => {
@@ -8,9 +8,27 @@ test('entering an invalid value shows an error message', () => {
     <FavoriteNumber />,
   )
   const input = getByLabelText(/favorite number/i)
+  // fireEvent.change(input, {target: {value: '10'}})
   user.type(input, '10')
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
   debug()
   rerender(<FavoriteNumber max={10} />)
   debug()
 })
+
+// import React from 'react'
+// import user from '@testing-library/user-event'
+// import {render} from '@testing-library/react'
+// import {FavoriteNumber} from '../favorite-number'
+
+// test('entering an invalid value shows an error message', () => {
+//   const {getByLabelText, getByRole, rerender, debug} = render(
+//     <FavoriteNumber />,
+//   )
+//   const input = getByLabelText(/favorite number/i)
+//   user.type(input, '10')
+//   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
+//   debug()
+//   rerender(<FavoriteNumber max={10} />)
+//   debug()
+// })
