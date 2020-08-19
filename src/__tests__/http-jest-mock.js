9998576666c13dc7d98bafab220a99d2f767db6b
olympiawoj <owojcik7@gmail.com>
2020-08-19 17:12:21 -0400

    12. Mock HTTP Requests with jest.mock in React Component Tests

--- src/__tests__/http-jest-mock.js
+++ src/__tests__/http-jest-mock.js
@@ -1,6 +1,28 @@
+// import React from 'react'
+// import {render, fireEvent, wait} from '@testing-library/react'
+// import {loadGreeting as mockLoadGreeting} from '../api'
+// import {GreetingLoader} from '../greeting-loader-01-mocking'
+
+// jest.mock('../api')
+
+// test('loads greetings on click', async () => {
+//   const testGreeting = 'TEST_GREETING'
+//   mockLoadGreeting.mockResolvedValueOnce({data: {greeting: testGreeting}})
+//   const {getByLabelText, getByText} = render(<GreetingLoader />)
+//   const nameInput = getByLabelText(/name/i)
+//   const loadButton = getByText(/load/i)
+//   nameInput.value = 'Mary'
+//   fireEvent.click(loadButton)
+//   expect(mockLoadGreeting).toHaveBeenCalledWith('Mary')
+//   expect(mockLoadGreeting).toHaveBeenCalledTimes(1)
+//   await wait(() =>
+//     expect(getByLabelText(/greeting/i)).toHaveTextContent(testGreeting),
+//   )
+// })
+
 import React from 'react'
-import {render, fireEvent, wait} from '@testing-library/react'
-import {loadGreeting as mockLoadGreeting} from '../api'
+import {render, fireEvent, wait} from '@testing-library/react' // wait is an async utility
+import {loadGreeting as mockLoadGreeting} from '../api' // mocked version of loadGreeting
 import {GreetingLoader} from '../greeting-loader-01-mocking'
 
 jest.mock('../api')
@@ -12,10 +34,13 @@ test('loads greetings on click', async () => {
   const nameInput = getByLabelText(/name/i)
   const loadButton = getByText(/load/i)
   nameInput.value = 'Mary'
-  fireEvent.click(loadButton)
+  fireEvent.click(loadButton) // when we click loadButton, we make a call to loadGreeting in the api module & ultimately it makes a server request
+  // we don't want to do that in a test, so let's let's mock this loadGreeting module
+  // now when we click, we make a request to mockLoadGreeting
+
   expect(mockLoadGreeting).toHaveBeenCalledWith('Mary')
   expect(mockLoadGreeting).toHaveBeenCalledTimes(1)
   await wait(() =>
     expect(getByLabelText(/greeting/i)).toHaveTextContent(testGreeting),
-  )
+  ) // wait is an async function so await it - checking if greeting is in DOM node
 })
