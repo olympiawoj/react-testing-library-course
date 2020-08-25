ad2cc1ad3d8cfc4c3c7fb3ccf5e26529ca902956
olympiawoj <owojcik7@gmail.com>
2020-08-25 12:42:15 -0400

    13. Mock HTTP Requests with Dependency Injection in React Component Tests

--- src/__tests__/dependency-injection.js
+++ src/__tests__/dependency-injection.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import {render, fireEvent, wait} from '@testing-library/react'
+// import {loadGreeting as mockLoadGreeting} from '../api'
 import {GreetingLoader} from '../greeting-loader-02-dependency-injection'
 
+// jest.mock('../api')
+
 test('loads greetings on click', async () => {
-  const mockLoadGreeting = jest.fn()
+  const mockLoadGreeting = jest.fn() // mock function and pass as prop to GreetingLoader
   const testGreeting = 'TEST_GREETING'
   mockLoadGreeting.mockResolvedValueOnce({data: {greeting: testGreeting}})
   const {getByLabelText, getByText} = render(
