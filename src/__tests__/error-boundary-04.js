eeba38ad715ae881b0baf771c743987a610631c6
olympiawoj <owojcik7@gmail.com>
2020-08-31 20:53:56 -0400

    18. Use React Testing Library's Wrapper Option to Simplify using rerender

--- src/__tests__/error-boundary-04.js
+++ src/__tests__/error-boundary-04.js
@@ -1,22 +1,86 @@
+// import React from 'react'
+// import {render, fireEvent} from '@testing-library/react'
+// import {reportError as mockReportError} from '../api'
+// import {ErrorBoundary} from '../error-boundary'
+
+// jest.mock('../api')
+
+// beforeAll(() => {
+//   jest.spyOn(console, 'error').mockImplementation(() => {})
+// })
+
+// afterAll(() => {
+//   console.error.mockRestore()
+// })
+
+// afterEach(() => {
+//   jest.clearAllMocks()
+// })
+
+// function Bomb({shouldThrow}) {
+//   if (shouldThrow) {
+//     throw new Error('💣')
+//   } else {
+//     return null
+//   }
+// }
+
+// test('calls reportError and renders that there was a problem', () => {
+//   mockReportError.mockResolvedValueOnce({success: true})
+//   const {rerender, getByText, queryByText, getByRole, queryByRole} = render(
+//     <Bomb />,
+//     {wrapper: ErrorBoundary},
+//   )
+
+//   rerender(<Bomb shouldThrow={true} />)
+
+//   const error = expect.any(Error)
+//   const info = {componentStack: expect.stringContaining('Bomb')}
+//   expect(mockReportError).toHaveBeenCalledWith(error, info)
+//   expect(mockReportError).toHaveBeenCalledTimes(1)
+
+//   expect(console.error).toHaveBeenCalledTimes(2)
+
+//   expect(getByRole('alert').textContent).toMatchInlineSnapshot(
+//     `"There was a problem."`,
+//   )
+
+//   console.error.mockClear()
+//   mockReportError.mockClear()
+
+//   rerender(<Bomb />)
+
+//   fireEvent.click(getByText(/try again/i))
+
+//   expect(mockReportError).not.toHaveBeenCalled()
+//   expect(console.error).not.toHaveBeenCalled()
+//   expect(queryByRole('alert')).not.toBeInTheDocument()
+//   expect(queryByText(/try again/i)).not.toBeInTheDocument()
+// })
+
 import React from 'react'
 import {render, fireEvent} from '@testing-library/react'
-import {reportError as mockReportError} from '../api'
+import {reportError as mockReportError} from '../api' // communicates what reportError is so alias as mockReportError which needs to have a mock implementation
 import {ErrorBoundary} from '../error-boundary'
 
-jest.mock('../api')
+jest.mock('../api') // mock out so we don't make API calls in our test, default mock finds ALL functions exported from api module  & replaces with jest functions
 
+// before all of the tests run, let's mock console.error implementation to do nothing
 beforeAll(() => {
   jest.spyOn(console, 'error').mockImplementation(() => {})
 })
 
+// clean up after all tests finish
 afterAll(() => {
-  console.error.mockRestore()
+  console.error.mockRestore() //restore console.error to original implementation
+  console.error('hi') // here I will get error output
 })
 
 afterEach(() => {
-  jest.clearAllMocks()
+  jest.clearAllMocks() // ensures all mocks we have by default from API have all been cleared after every one of the tests
 })
 
+// test ONLY component, Bomb
 function Bomb({shouldThrow}) {
   if (shouldThrow) {
     throw new Error('💣')
@@ -26,32 +90,30 @@ function Bomb({shouldThrow}) {
 }
 
 test('calls reportError and renders that there was a problem', () => {
-  mockReportError.mockResolvedValueOnce({success: true})
-  const {rerender, getByText, queryByText, getByRole, queryByRole} = render(
+  mockReportError.mockResolvedValueOnce({success: true}) // returns a promise, so mock resolve value- mock out to w/e server shiould be responding with
+  // ensures our mock of function resolves to success: true
+  const {rerender, getByText, getByRole, queryByRole, queryByText} = render(
     <Bomb />,
-    {wrapper: ErrorBoundary},
-  )
+    {wrapper: ErrorBoundary}, // wrapper option = this will take the UI we provide in 1st arg and wraps it in the ErrorBoundary component.
+  ) // render with Bomb component
+  rerender(<Bomb shouldThrow={true} />) // when we do this, we makes a call to reportError, throw error, componentDidCatch should run, re render with error info
+  // ANY time we call rerender, this Bomb will be wrapped in the ErrorBoundary
 
-  rerender(<Bomb shouldThrow={true} />)
-
-  const error = expect.any(Error)
+  const error = expect.any(Error) // asymmetric matcher
   const info = {componentStack: expect.stringContaining('Bomb')}
   expect(mockReportError).toHaveBeenCalledWith(error, info)
   expect(mockReportError).toHaveBeenCalledTimes(1)
-
-  expect(console.error).toHaveBeenCalledTimes(2)
+  expect(console.error).toHaveBeenCalledTimes(2) // once by jest-dom, once by react-dom
 
   expect(getByRole('alert').textContent).toMatchInlineSnapshot(
     `"There was a problem."`,
-  )
+  ) // render alert when error was thrown
 
   console.error.mockClear()
-  mockReportError.mockClear()
+  mockReportError.mockClear() // resets calls to 0, leaves mock implementation
 
   rerender(<Bomb />)
-
   fireEvent.click(getByText(/try again/i))
-
   expect(mockReportError).not.toHaveBeenCalled()
   expect(console.error).not.toHaveBeenCalled()
   expect(queryByRole('alert')).not.toBeInTheDocument()
