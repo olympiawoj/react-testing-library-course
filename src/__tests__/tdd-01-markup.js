68efcbd0f8067e34ff7ee99a2e145b9954345162
olympiawoj <owojcik7@gmail.com>
2020-09-04 12:18:43 -0400

    19. Test Drive the Development of a React Form with React Testing Library

--- src/__tests__/tdd-01-markup.js
+++ src/__tests__/tdd-01-markup.js
@@ -1,6 +1,18 @@
-import React from 'react'
-import {render} from '@testing-library/react'
-import {Editor} from '../post-editor-01-markup'
+// import React from 'react'
+// import {render} from '@testing-library/react'
+// import {Editor} from '../post-editor-01-markup'
+
+// test('renders a form with title, content, tags, and a submit button', () => {
+//   const {getByLabelText, getByText} = render(<Editor />)
+//   getByLabelText(/title/i)
+//   getByLabelText(/content/i)
+//   getByLabelText(/tags/i)
+//   getByText(/submit/i)
+// })
+
+import React from 'react' // need React to create the Editor element
+import {render} from '@testing-library/react' // to render the Editor element
+import {Editor} from '../post-editor-01-markup' // import the Editor element
 
 test('renders a form with title, content, tags, and a submit button', () => {
   const {getByLabelText, getByText} = render(<Editor />)
