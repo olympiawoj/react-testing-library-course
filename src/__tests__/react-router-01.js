42706f8e5782c4adef2dc02759fbcad1db9c9669
olympiawoj <owojcik7@gmail.com>
2020-09-07 15:19:27 -0400

    27. Test React Components that Use the react-router Router Provider with createMemoryHistory

--- src/__tests__/react-router-01.js
+++ src/__tests__/react-router-01.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import {Router} from 'react-router-dom'
-import {createMemoryHistory} from 'history'
+import {createMemoryHistory} from 'history' // create my own history and specify initial entries for history
 import {render, fireEvent} from '@testing-library/react'
 import {Main} from '../main'
 
 test('main renders about and home and I can navigate to those pages', () => {
-  const history = createMemoryHistory({initialEntries: ['/']})
-  const {getByRole, getByText} = render(
+  const history = createMemoryHistory({
+    initialEntries: ['/'], // home page
+  })
+  const {getByRole, getByText, debug} = render(
     <Router history={history}>
       <Main />
     </Router>,
   )
+
   expect(getByRole('heading')).toHaveTextContent(/home/i)
+  debug()
   fireEvent.click(getByText(/about/i))
+  debug()
   expect(getByRole('heading')).toHaveTextContent(/about/i)
 })
