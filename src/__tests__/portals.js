cd933b05f1f31348241f294e15f553430d1426b8
olympiawoj <owojcik7@gmail.com>
2020-09-08 12:51:38 -0400

    37. Test React Portals with within from React Testing Library

--- src/__tests__/portals.js
+++ src/__tests__/portals.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import {render, within} from '@testing-library/react'
+import {render, within, queries} from '@testing-library/react'
 import {Modal} from '../modal'
 
 test('modal shows the children', () => {
+  // div is NOT appearing inside of the container that render creates for us
+  // might think we need something special to get access to div
+  // but getByTestId and all queries by DEFAULT are bound to document.body
   render(
-    <Modal>
-      <div data-testid="test" />
-    </Modal>,
+    <>
+      <div data-testid="foo" />
+      <Modal>
+        <div data-testid="test" />
+      </Modal>
+    </>,
   )
-  const {getByTestId} = within(document.getElementById('modal-root'))
+
+  // bound to this element rather than body
+  const {getByTestId} = within(document.getElementById('modal-root')) // get specific queries within the modal root
+  queries.getByTestId(document.body, 'foo') // container as 1st arg
   expect(getByTestId('test')).toBeInTheDocument()
 })
